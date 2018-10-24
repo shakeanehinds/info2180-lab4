ec7be6480b11d402446a4e4c21513a4dd0a79288
shakeanehinds <shakeanehinds@users.noreply.github.com>
2018-10-23 23:58:15 -0500

    Update maze.js

---
+++ maze.js
@@ -0,0 +1,12 @@
+window.onload;
+
+let boundaryOver = document.getElementById("boundary1");
+
+boundaryOver.addEventListener("mouseover", function warning() 
+	{
+		boundaryOver.classList.add("youlose");
+	}
+)
+
+			
+

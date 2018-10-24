3401b05fe51c94f91ceb50323944ffc9e44a1f69
shakeanehinds <shakeanehinds@users.noreply.github.com>
2018-10-23 23:58:44 -0500

    Update maze.js

--- maze.js
+++ maze.js
@@ -10,3 +10,28 @@ boundaryOver.addEventListener("mouseover", function warning()
 
 			
 
+
+for ( var count = 0; count < document.getElementsByClassName("boundary").length - 1; count++)
+		{
+
+			document.getElementsByClassName("boundary")[count].addEventListener("mouseover", function control()
+					{
+						for (var n = 0; n < document.getElementsByClassName("boundary").length - 1; n++)
+						{
+		
+						document.querySelectorAll("div.boundary")[n].classList.add("youlose");
+
+						}
+
+							let newElement = document.createElement("h2");
+							newElement.textContent = "You Lose Bredda!";
+						    document.body.append(newElement);
+						    var status = document.createAttribute("id");
+							status.value = "status";
+							newElement.setAttributeNode(status);	
+
+						 
+					})
+		}
+
+
